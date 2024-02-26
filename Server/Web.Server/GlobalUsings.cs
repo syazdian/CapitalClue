0a98e720b967533a9ddc3f31e397eb1938c6b074
arbabsadeghi <m.arbabsadeghi@gmail.com>
2024-02-26 16:34:03 -0500

    fix a bug

--- Server/Web.Server/GlobalUsings.cs
+++ Server/Web.Server/GlobalUsings.cs
@@ -2,5 +2,4 @@
 global using Microsoft.AspNetCore.Mvc;
 global using CapitalClue.Common.Models;
 global using Microsoft.AspNetCore.Components.Web;
-global using CapitalClue.Web.Server.Services;
 global using CapitalClue.Web.Server.Ml.Stock.ModelBuilder;
