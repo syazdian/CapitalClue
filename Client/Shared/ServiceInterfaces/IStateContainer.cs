f18f198314967fa04396fc014033d76f2a686181
arbabsadeghi <m.arbabsadeghi@gmail.com>
2024-03-04 16:27:55 -0500

    For resolve conflicts

--- Client/Shared/ServiceInterfaces/IStateContainer.cs
+++ Client/Shared/ServiceInterfaces/IStateContainer.cs
@@ -5,4 +5,21 @@ public interface IStateContainer
     public List<ChartDataItem> revenueProperty { get; set; }
     public List<ChartDataItem> revenuePropertyUpperBound { get; set; }
     public List<ChartDataItem> revenuePropertyLowerBound { get; set; }
+
+    public double PropertyDownpayment { get; set; }
+    public double PropertyMonthlyPayment { get; set; }
+    public double PropertyMonthlyRent { get; set; }
+    public double StockContribution { get; set; }
+    public double StockDownpayment { get; set; }
+
+    public double houseCost { get; set; }
+    public double interestRate { get; set; }
+    public double mortgagePayment { get; set; }
+    public double propertyTax { get; set; }
+    public double homeInsurance { get; set; }
+    public double maintananceFee { get; set; }
+    public int mortagePaymentPeriod { get; set; }
+
+    public string PropertyType { get; set; }
+    public string City { get; set; }
 }
--- Client/Web/Services/Models/StateContainer.cs
+++ Client/Web/Services/Models/StateContainer.cs
@@ -6,4 +6,22 @@ public class StateContainer : IStateContainer
     public List<ChartDataItem> revenueProperty { get; set; }
     public List<ChartDataItem> revenuePropertyUpperBound { get; set; }
     public List<ChartDataItem> revenuePropertyLowerBound { get; set; }
+
+    public double PropertyDownpayment { get; set; }
+    public double PropertyMonthlyPayment { get; set; }
+    public double PropertyMonthlyRent { get; set; }
+
+    public double StockContribution { get; set; }
+    public double StockDownpayment { get; set; }
+
+    public double houseCost { get; set; } = 0;
+    public double interestRate { get; set; } = 0;
+    public double mortgagePayment { get; set; } = 0;
+    public double propertyTax { get; set; } = 0;
+    public double homeInsurance { get; set; } = 0;
+    public double maintananceFee { get; set; } = 0;
+    public int mortagePaymentPeriod { get; set; } = 0;
+
+    public string PropertyType { get; set; }
+    public string City { get; set; }
 }
