ff734e70a3aed5cdfa827dd19a21bd106c5b7a55
arbabsadeghi <m.arbabsadeghi@gmail.com>
2024-05-06 19:09:04 -0400

    select stock type before prediction

--- Client/Shared/ServiceInterfaces/IFetchData.cs
+++ Client/Shared/ServiceInterfaces/IFetchData.cs
@@ -3,7 +3,7 @@
 public interface IFetchData
 {
     public Task<PropertyPredictionDto> GetPropertyPredicionPercent(string city, string propertyType);
-    public Task<StockPredictionDto> GetStockPredicionPercent(string city, string propertyType);
+    public Task<StockPredictionDto> GetStockPredicionPercent(string StockName, string Currency = "US");
     public Task<StockPredictionDto> GetStockPredicionPercent();
 
 }
