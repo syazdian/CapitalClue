c76ba8bf2c0210f131655b1413b58fa64bd4666a
Sharif Yazdian <Sharif.Yazdian@staples.ca>
2024-05-20 16:35:52 -0400

    Add azur eFunction

---
+++ ML/CapitalClue-AzF/PropertyTrainCreateModel.cs
@@ -0,0 +1,44 @@
+using CapitalClue.Common.Models;
+using CapitalClue.Web.Server.Ml.Property.ModelBuilder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CapitalClue_AzF
+{
+    public class PropertyTrainCreateModel
+    {
+        private readonly ILogger<PropertyTrainCreateModel> _logger;
+
+        public PropertyTrainCreateModel(ILogger<PropertyTrainCreateModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("PropertyTrainCreateModel")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            try
+            {
+                string requestBody;
+                using (StreamReader reader = new StreamReader(req.Body))
+                {
+                    requestBody = reader.ReadToEnd();
+                }
+
+                var propertyModel = JsonConvert.DeserializeObject<PropertyModelDto>(requestBody);
+
+                var modelBulder = new PropertyModelBuilder(propertyModel);
+                modelBulder.Build();
+                return new OkObjectResult($"successfully build Property Model{propertyModel.City}- {propertyModel.PropertyType}");
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError($"TrainAndMakeModelStock : {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ ML/CapitalClue-AzF/StockTrainCreateModel.cs
@@ -0,0 +1,45 @@
+using CapitalClue.Common.Models;
+using CapitalClue.Web.Server.Ml.Property.ModelBuilder;
+using CapitalClue.Web.Server.Ml.Stock.ModelBuilder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CapitalClue_AzF
+{
+    public class StockTrainCreateModel
+    {
+        private readonly ILogger<StockTrainCreateModel> _logger;
+
+        public StockTrainCreateModel(ILogger<StockTrainCreateModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("StockTrainCreateModel")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            try
+            {
+                string requestBody;
+                using (StreamReader reader = new StreamReader(req.Body))
+                {
+                    requestBody = reader.ReadToEnd();
+                }
+
+                var stockModel = JsonConvert.DeserializeObject<StockModelDto>(requestBody);
+
+                var modelBuilder = new StockModelBuilder(stockModel);
+                modelBuilder.Build();
+                return new OkObjectResult($"successfully build Property Model{stockModel.StockName}-{stockModel.Currency} ");
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError($"TrainAndMakeModelStock : {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
